b6dee45ee94295877a6391e8066cbc2f1b888068
Olzhas Suleimen <amajisenpai@gmail.com>
2023-01-05 23:32:58 +0600

    sync(astra): 1.0.0-dev.139

--- packages/astra/CHANGELOG.md
+++ packages/astra/CHANGELOG.md
@@ -1,3 +1,3 @@
-## 1.0.0-dev.138
+## 1.0.0-dev.139
 
 - Initial version.
--- packages/astra/lib/src/isolate/isolate.dart
+++ packages/astra/lib/src/isolate/isolate.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:io' show InternetAddress;
-import 'dart:isolate' show RawReceivePort, ReceivePort, SendPort;
+import 'dart:isolate' show ReceivePort, SendPort;
 
 import 'package:astra/core.dart';
 import 'package:astra/src/isolate/message.dart';
