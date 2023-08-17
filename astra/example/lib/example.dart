dbfdc2afbfc65799850bf3bf2a6b7fe60d0e8e8c
Olzhas Suleimen <amajisenpai@gmail.com>
2023-08-17 20:37:33 +0600

    sync(example): 1.0.0+4

--- astra/example/lib/example.dart
+++ astra/example/lib/example.dart
@@ -1,38 +1,32 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:astra/core.dart';
+import 'package:astra/middlewares.dart';
 
-Application get application => Example();
+Application get application {
+  return Counter();
+}
 
-class Example extends Application {
+class Counter extends Application {
   int count = 0;
 
   @override
   Handler get entryPoint {
-    return (Request request) async {
-      count += 1;
+    Future<Response> handler(Request request) async {
+      return Response.ok('You have requested this application ${++count} time(s).');
+    }
 
-      return switch (request.url.path) {
-        '' => Response.ok('hello world!'),
-        'count' => Response.ok('count: $count'),
-        'readme' => Response.ok(File('README.md').openRead()),
-        'error' => throw Exception('some message'),
-        _ => Response.notFound('Request for "${request.url}"'),
-      };
-    };
+    return handler.use(logRequests());
   }
 
   @override
   Future<void> reload() async {
+    print('Application reloaded.');
     count = 0;
-    // ...
   }
 
   @override
   Future<void> close() async {
-    // ignore: avoid_print
-    print('closing ...');
-    // ...
+    print('Application closed.');
   }
 }
